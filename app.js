76fd91cb23444c36ffa851d89c0a42769a76ae9a
Abed Almajeed Mansour <abdelmajeed9@gmail.com>
2018-10-18 22:51:26 +0000

    PORT

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.use("/campgrounds/:id/comment", commentRoutes);
 //     console.log("The YelpCamp server has started!");
 // });
 
-app.listen(process.env.port, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp server has started!");
 });
